7768698c88ac4f6ff17ecfc84b41814bc979e1dc
Emmanuel <rojaz813@gmail.com>
2020-09-06 20:20:07 +0300

    Prevent search form submission on empty input

--- src/js/modules/ui.js
+++ src/js/modules/ui.js
@@ -314,6 +314,7 @@ export default class UI {
 
       searchFormEl.onsubmit = (e) => {
          e.preventDefault();
+         if (!query.value) return;
          action(query.value);
       };
    }
